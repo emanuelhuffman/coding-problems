555d367bfec3602340c896be05c58c7cd71f9af9
Emanuel Huffman <ehvdb@mst.edu>
2022-08-20 23:32:09 -0500

    prod arr solve

---
+++ prodArrExSelf.js
@@ -0,0 +1,14 @@
+var productExceptSelf = function(nums) {
+    let res = new Array(nums.length)
+    res[0] = 1
+    for(let i = 1; i < nums.length; i++){
+        res[i] = res[i - 1] * nums[i - 1]
+    }
+    let right = 1;
+    for(let i = nums.length - 1; i >= 0; i--){
+        res[i] *= right
+        right *= nums[i]
+    }
+    
+    return res
+};
